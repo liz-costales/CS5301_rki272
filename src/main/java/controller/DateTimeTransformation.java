91c123069fdd7972cc82ee1fc137e4fbe948ccdd
Elizabeth Costales <ep1085@gmail.com>
2023-04-02 00:12:32 -0500

    Working on updates to include Day of the Week in the returned value. Fixed a bug for the input format.

--- src/main/java/controller/DateTimeTransformation.java
+++ src/main/java/controller/DateTimeTransformation.java
@@ -11,10 +11,15 @@ public class DateTimeTransformation {
 
         ZonedDateTime convertedDateTime;
         String timeZone = ZoneId.SHORT_IDS.get(givenTimeZone);
+        String convertedDateTime2;
 
         try {
             DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss").withZone(ZoneId.of(timeZone));
             convertedDateTime = ZonedDateTime.parse(givenDateTime, fmt);
+            DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("EEEE MM-dd-yyyy HH:mm:ss").withZone(ZoneId.of(timeZone));
+            convertedDateTime2 = convertedDateTime.format(fmt2);
+
+            System.out.println("Date with DOW is: " + convertedDateTime2);
 
         } catch (Exception e) {
             System.out.println("The datetime or timezone string provided was null or invalid. Cannot convert " +
@@ -22,15 +27,14 @@ public class DateTimeTransformation {
             throw e;
         }
 
-//        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
-        return convertedDateTime;
+         return convertedDateTime;
     }
     public boolean isDateTimeValid(String givenDateTime)
     {
         if(givenDateTime != null && givenDateTime.length() == 19)
         {
             try {
-                DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss").parse(givenDateTime).toString();
+                DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss").parse(givenDateTime).toString();
                 return true;
             }
             catch (DateTimeParseException e) {
